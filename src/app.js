b2ea3c38c16774a212c61fd3aac51646e6ab5391
nya1 <nya1@protonmail.com>
2019-04-01 20:56:17 +0200

    Add comments

--- src/app.js
+++ src/app.js
@@ -4,10 +4,12 @@ const route = require('koa-route')
 const checkEmail = require('./check-email.js')
 const PORT = process.env.PORT || 3000
 const app = new Koa()
-app.use(logger()) // start simple logger
+app.use(logger()) // attach logger
 
+// set main endpoint
 app.use(route.get('/check/:email', checkEmail))
 
+// check if is running under test env.
 if (process.env.NODE_ENV !== 'test') {
   app.listen(PORT)
   console.log(`* email-ok listening on port ${PORT}`)
